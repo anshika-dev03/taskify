fb45da588d73d587683386e0a19d37fbc2e53628
anshika-dev03 <anshikapopli1011@gmail.com>
2025-08-20 21:28:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173", "https://taskify-black-eta.vercel.app/"
     ],
     credentials: true,
   })
