8b2cfafadae8ab3cad1181304c34ab242636bd57
anshika-dev03 <anshikapopli1011@gmail.com>
2025-08-20 21:35:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,36 @@
 const express = require("express");
 const app = express();
-
 require("dotenv").config();
 require("./connection/conn");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const path=require("path");
-app.use(cookieParser());
+const path = require("path");
 
+app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", "https://taskify-black-eta.vercel.app/"
+      "http://localhost:5173", 
+      "https://taskify-black-eta.vercel.app"  // Remove trailing slash
     ],
     credentials: true,
   })
 );
 
+app.use(express.json());
 
+// API routes
 const usersApi = require("./controllers/user");
 const tasksApi = require("./controllers/task");
-
-app.use(express.json());
 app.use("/api/v1", usersApi);
 app.use("/api/v1", tasksApi);
 
+// Serve static files from React build
+app.use(express.static(path.resolve(__dirname, "client", "dist")));
 
-// app.get("/", (req, res) => {
-//   app.use(express.static (path.resolve(__dirname, "client", "dist"))); 
-//   res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-// });
-// app.use(express.static(path.resolve(__dirname, "client", "dist")));
+// Catch-all handler for React routing
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-// });
-// if(require.main ===module){
-//  app.listen(process.env.PORT, () => {
-//    console.log(`Server Started : ${process.env.PORT}`);
-//  });
-// }
-module.exports=app;
+module.exports = app;
